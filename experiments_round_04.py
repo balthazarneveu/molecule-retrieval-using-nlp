003c222c690719cf2660acd7d6f6d3b008c104f7
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-02 23:32:14 +0100

    exp 400 increase number of experiments

--- experiments_round_04.py
+++ experiments_round_04.py
@@ -15,7 +15,7 @@ def get_round_4_experience(exp: int, configuration: dict, root_dir: Path = None,
     assert exp >= 400 and exp <= 499, "round 4 between 400 and 499"
     if exp == 400:
         configuration[BATCH_SIZE] = (32, 32, 32)    # RTX2060
-        configuration[NB_EPOCHS] = 60
+        configuration[NB_EPOCHS] = 150
         configuration[OPTIMIZER][LEARNING_RATE] = 7e-6
         configuration[OPTIMIZER][WEIGHT_DECAY] = 0.1
         configuration[NAME] = 'SciBERT-GCN'
