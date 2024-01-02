4b3afc690727e06414780ba33d9c0f869ae68a2e
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-02 22:45:01 +0100

    Exp 106 ADAPTER

--- experiments_round_01.py
+++ experiments_round_01.py
@@ -3,7 +3,7 @@ from properties import (
 )
 from pathlib import Path
 from multimodal_model import MultimodalModel
-from language_model import TextEncoder
+from language_model import TextEncoder, Adapter
 from graph_model import BasicGraphEncoder
 
 
@@ -58,4 +58,27 @@ def get_round_1_experience(exp: int, configuration: dict, root_dir: Path = None,
         graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
         text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
         model = MultimodalModel(graph_encoder, text_encoder)
+    if exp == 105:
+        configuration[BATCH_SIZE] = (32, 32, 32)
+        configuration[NB_EPOCHS] = 60
+        configuration[OPTIMIZER][LEARNING_RATE] = 5e-5
+        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+        configuration[NAME] = 'FBERT-GCN'
+        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
+        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
+        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
+        model = MultimodalModel(graph_encoder, text_encoder)
+    if exp == 106:
+        configuration[BATCH_SIZE] = (32, 32, 32)
+        configuration[NB_EPOCHS] = 60
+        configuration[OPTIMIZER][LEARNING_RATE] = 1e-4
+        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+        configuration[NAME] = 'FBERT-ADAPT-GCN'
+        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
+        shared_out_size = 256
+        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=shared_out_size, nhid=300, graph_hidden_channels=300)
+        text_adapter = Adapter(text_embedding_size=768, out_size=shared_out_size)
+        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True, adapter=text_adapter)
+        model = MultimodalModel(graph_encoder, text_encoder)
+    
     return model, configuration
--- language_model.py
+++ language_model.py
@@ -1,21 +1,45 @@
 
 from transformers import AutoModel
 from generic import GenericModel
+import torch
+from typing import Optional
 
 
 class TextEncoder(GenericModel):
-    def __init__(self, model_name: str = 'distilbert-base-uncased', freeze: bool = True):
+    def __init__(
+        self, model_name: str = 'distilbert-base-uncased',
+        freeze: bool = True,
+        adapter: Optional[GenericModel] = None
+    ):
         super(TextEncoder, self).__init__()
         self.bert = AutoModel.from_pretrained(model_name)
         if freeze:
             for param in self.bert.parameters():
                 param.requires_grad = False
+        self.adapter = adapter
 
     def forward(self, input_ids, attention_mask):
         encoded_text = self.bert(input_ids, attention_mask=attention_mask)
-        return encoded_text.last_hidden_state[:, 0, :]
+        out = encoded_text.last_hidden_state[:, 0, :]
+        if self.adapter is not None:
+            out = self.adapter(out)
+        return out
+
+
+class Adapter(GenericModel):
+    def __init__(self, text_embedding_size=768, hdim=512, out_size=512):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(text_embedding_size, hdim)
+        self.fc2 = torch.nn.Linear(hdim, out_size)
+        self.relu = torch.nn.ReLU()
+        self.adapter = torch.nn.Sequential(self.fc1, self.relu, self.fc2)
+
+    def forward(self, x):
+        return self.adapter(x)
 
 
 if __name__ == '__main__':
-    text_encoder = TextEncoder()
+    adapt = Adapter()
+    # adapt = None
+    text_encoder = TextEncoder(adapter=adapt)
     print(text_encoder.count_params())
