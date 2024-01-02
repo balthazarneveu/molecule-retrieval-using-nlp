66a6d9e174770f5bcd50657b3bd159f70cc26406
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-02 22:45:01 +0100

    exp 104

--- experiments_round_01.py
+++ experiments_round_01.py
@@ -48,4 +48,14 @@ def get_round_1_experience(exp: int, configuration: dict, root_dir: Path = None,
         graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
         text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
         model = MultimodalModel(graph_encoder, text_encoder)
+    if exp == 104:
+        configuration[BATCH_SIZE] = (32, 32, 32)
+        configuration[NB_EPOCHS] = 60
+        configuration[OPTIMIZER][LEARNING_RATE] = 1e-5
+        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+        configuration[NAME] = 'FBERT-GCN'
+        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
+        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
+        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
+        model = MultimodalModel(graph_encoder, text_encoder)
     return model, configuration
