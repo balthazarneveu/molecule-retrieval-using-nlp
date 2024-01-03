31fbdb8dd46721e93a5d69d2279341acca1a9737
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-03 10:53:34 +0100

    factorize code for round 1 (100, 101, 102, 103, 104, 105) FBERT + base GCN (106, 110, 112) F(sci)BERT+adapter + base GCN

--- experiments_round_01.py
+++ experiments_round_01.py
@@ -9,73 +9,53 @@ from graph_model import BasicGraphEncoder, BigGraphEncoder
 
 def get_round_1_experience(exp: int, configuration: dict, root_dir: Path = None, backup_root: Path = None):
     """Freeze BERT and train adapter + base GCN"""
-    if exp == 100:
-        configuration[BATCH_SIZE] = (32, 32, 32)
-        configuration[NB_EPOCHS] = 60
-        configuration[OPTIMIZER][LEARNING_RATE] = 5e-5
-        configuration[OPTIMIZER][WEIGHT_DECAY] = 1.  # OOPS!!!!!!!!!!!!!!!!
+    if exp in [100, 101, 102, 103, 104, 105]:
+        # Base GCN - Frozen BERT
         configuration[NAME] = 'FBERT-GCN'
         configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
         graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
         text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
         model = MultimodalModel(graph_encoder, text_encoder)
-    if exp == 101:
         configuration[BATCH_SIZE] = (32, 32, 32)
+        configuration[NB_EPOCHS] = 60
+        if exp == 100:
+            configuration[OPTIMIZER][LEARNING_RATE] = 5e-5
+            configuration[OPTIMIZER][WEIGHT_DECAY] = 1.  # OOPS!!!!!!!!!!!!!!!!
+        if exp == 101:
+            configuration[OPTIMIZER][LEARNING_RATE] = 1e-4
+            configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+        if exp == 102:
+            configuration[OPTIMIZER][LEARNING_RATE] = 5e-4
+            configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+
+        if exp == 103:
+            configuration[OPTIMIZER][LEARNING_RATE] = 1e-3
+            configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+
+        if exp == 104:
+            configuration[OPTIMIZER][LEARNING_RATE] = 1e-5
+            configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+
+        if exp == 105:
+            configuration[OPTIMIZER][LEARNING_RATE] = 5e-5
+            configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
+
+    if exp in [106, 110, 112]:
+        # Frozen BERT or SciBERT + ADAPTER
+        # Base GCN
         configuration[NB_EPOCHS] = 60
         configuration[OPTIMIZER][LEARNING_RATE] = 1e-4
         configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
-        configuration[NAME] = 'FBERT-GCN'
-        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
-        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
-        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
-        model = MultimodalModel(graph_encoder, text_encoder)
-    if exp == 102:
         configuration[BATCH_SIZE] = (32, 32, 32)
-        configuration[NB_EPOCHS] = 60
-        configuration[OPTIMIZER][LEARNING_RATE] = 5e-4
-        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
-        configuration[NAME] = 'FBERT-GCN'
-        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
-        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
-        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
-        model = MultimodalModel(graph_encoder, text_encoder)
-    if exp == 103:
-        configuration[BATCH_SIZE] = (32, 32, 32)
-        configuration[NB_EPOCHS] = 60
-        configuration[OPTIMIZER][LEARNING_RATE] = 1e-3
-        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
-        configuration[NAME] = 'FBERT-GCN'
-        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
-        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
-        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
-        model = MultimodalModel(graph_encoder, text_encoder)
-    if exp == 104:
-        configuration[BATCH_SIZE] = (32, 32, 32)
-        configuration[NB_EPOCHS] = 60
-        configuration[OPTIMIZER][LEARNING_RATE] = 1e-5
-        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
-        configuration[NAME] = 'FBERT-GCN'
-        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
-        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
-        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
-        model = MultimodalModel(graph_encoder, text_encoder)
-    if exp == 105:
-        configuration[BATCH_SIZE] = (32, 32, 32)
-        configuration[NB_EPOCHS] = 60
-        configuration[OPTIMIZER][LEARNING_RATE] = 5e-5
-        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
-        configuration[NAME] = 'FBERT-GCN'
-        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
-        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300)
-        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True)
-        model = MultimodalModel(graph_encoder, text_encoder)
-    if exp == 106:
-        configuration[BATCH_SIZE] = (32, 32, 32)
-        configuration[NB_EPOCHS] = 60
-        configuration[OPTIMIZER][LEARNING_RATE] = 1e-4
-        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
-        configuration[NAME] = 'FBERT-ADAPT-GCN'
-        configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
+        if exp == 106:  # Frozen BERT + ADAPTER
+            configuration[NAME] = 'FBERT-ADAPT-GCN'
+            configuration[ANNOTATIONS] = 'Frozen BERT - Trainable GCN'
+        if exp in [110, 112]:  # Frozen Sci BERT + ADAPTER
+            configuration[TOKENIZER_NAME] = "allenai/scibert_scivocab_uncased"
+            configuration[NAME] = 'F-SciBERT-ADAPT-GCN'
+            configuration[ANNOTATIONS] = 'Frozen SciBERT - Trainable GCN'
+            if exp == 110:
+                configuration[BATCH_SIZE] = (8, 8, 8)
         shared_out_size = 256
         graph_encoder = BasicGraphEncoder(num_node_features=300, nout=shared_out_size,
                                           nhid=300, graph_hidden_channels=300)
@@ -83,23 +63,8 @@ def get_round_1_experience(exp: int, configuration: dict, root_dir: Path = None,
         text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True, adapter=text_adapter)
         model = MultimodalModel(graph_encoder, text_encoder)
 
-    if exp == 110 or exp == 112:
-        configuration[TOKENIZER_NAME] = "allenai/scibert_scivocab_uncased"
-        configuration[BATCH_SIZE] = (8, 8, 8)
-        if exp == 112:
-            configuration[BATCH_SIZE] = (32, 32, 32)
-        configuration[NB_EPOCHS] = 60
-        configuration[OPTIMIZER][LEARNING_RATE] = 1e-4
-        configuration[OPTIMIZER][WEIGHT_DECAY] = 0.01
-        configuration[NAME] = 'F-SciBERT-ADAPT-GCN'
-        configuration[ANNOTATIONS] = 'Frozen SciBERT - Trainable GCN'
-        shared_out_size = 256
-        graph_encoder = BasicGraphEncoder(num_node_features=300, nout=shared_out_size,
-                                          nhid=300, graph_hidden_channels=300)
-        text_adapter = Adapter(text_embedding_size=768, out_size=shared_out_size)
-        text_encoder = TextEncoder(configuration[TOKENIZER_NAME], freeze=True, adapter=text_adapter)
-        model = MultimodalModel(graph_encoder, text_encoder)
     if exp == 111 or exp == 113:
+        # Bigger GCN
         configuration["GCN-architecture"] = {
             "depth": 5,
             "GCN-FC-hidden-size": 512,
