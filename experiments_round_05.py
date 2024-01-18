6cc4dca914055a67efb20443c19dce5c8619b525
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-18 10:20:58 +0100

    #19 fix exp 515

--- experiments_round_05.py
+++ experiments_round_05.py
@@ -121,7 +121,7 @@ def get_round_5_experience(exp: int, conf: dict, root_dir: Path = None, backup_r
     elif exp == 506:
         # LR 1e-3 is too high
         model, conf = lora_exp(conf, b=32, n=20, lr=1e-3, wd=0.1, model_name="distilbert")
-    elif exp == 507 or exp == 511 or exp == 512:
+    elif exp in [507, 511, 512, 515]:
         # Seems to have a good convergence
         # begining looks as good as training all BERT parameters with mega batch size 128 - exp 68
         if exp == 507:
