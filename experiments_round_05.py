16c4d509db190110aeed1e4a3ea4928d44b13928
lea-khalil <leakhalil@yahoo.fr>
2024-01-18 13:36:41 +0100

    smaller batch_size exp 502

--- experiments_round_05.py
+++ experiments_round_05.py
@@ -100,7 +100,7 @@ def get_round_5_experience(exp: int, conf: dict, root_dir: Path = None, backup_r
     elif exp == 501:
         model, conf = lora_exp(conf, b=32, n=60, lr=7e-6, wd=0.1, model_name="scibert")
     elif exp == 502:
-        model, conf = lora_exp(conf, b=256, n=200, lr=3e-4, wd=0.1, model_name="scibert")  # probably A5000 required
+        model, conf = lora_exp(conf, b=128, n=200, lr=3e-4, wd=0.1, model_name="scibert")  # probably A5000 required
     elif exp == 503:
         graph_encoder = BigGraphEncoder(num_node_features=300, nout=768, nhid=256, graph_hidden_channels=512)
         model, conf = lora_exp(conf, b=128, n=150, lr=7e-6, wd=0.1, model_name="scibert", graph_encoder=graph_encoder)
