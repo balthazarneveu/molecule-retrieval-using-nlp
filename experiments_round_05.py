011eb940c480df08cb3f2a11df55531e38b8cb3c
lea-khalil <leakhalil@yahoo.fr>
2024-01-22 15:23:05 +0100

    exp 572 fat-gcn+scibert +scheduler lr=3e-4

--- experiments_round_05.py
+++ experiments_round_05.py
@@ -212,6 +212,13 @@ def get_round_5_experience(exp: int, conf: dict, root_dir: Path = None, backup_r
         conf[SCHEDULER_CONFIGURATION] = dict(patience=5, factor=0.5)
         conf[ANNOTATIONS] += "- bigger GCN"
         conf[NAME] = conf[NAME].replace("GCN", "biggerGCN")
+    elif exp == 572:  # try scibert +fat-gcn +scheduler plateau
+        graph_encoder = FatGraphEncoder(num_node_features=300, nout=768, nhid=256, graph_hidden_channels=512)
+        model, conf = lora_exp(conf, b=64, n=150, lr=3e-4, wd=0.1, model_name="scibert", graph_encoder=graph_encoder)
+        conf[SCHEDULER] = "ReduceLROnPlateau"
+        conf[SCHEDULER_CONFIGURATION] = dict(patience=5, factor=0.5)
+        conf[ANNOTATIONS] += "- fat GCN"
+        conf[NAME] = conf[NAME].replace("GCN", "FatGCN")
     else:
         raise NameError(f"Experiment {exp} not implemented")
 
