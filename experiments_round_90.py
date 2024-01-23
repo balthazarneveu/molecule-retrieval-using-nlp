c82989ef45e7c25755c17bbd258dfbe1491e9190
lea-khalil <leakhalil@yahoo.fr>
2024-01-23 18:00:49 +0100

    smaller bs exp 9003

--- experiments_round_90.py
+++ experiments_round_90.py
@@ -47,7 +47,7 @@ def get_round_90_experience(exp: int, configuration: dict, root_dir: Path = None
             configuration,
             llm=SCIBERT, graph=FAT_GCN,
             n=200,
-            b=128, lr=1e-4, wd=1e-1,
+            b=96, lr=3e-4, wd=1e-1,
             scheduler=PLATEAU, scheduler_configuration=dict(patience=10, factor=0.8),
             lora=False, quantization=None
         )
