ec66ea96778e27781159b0156e8707dc2b3b7833
Basile-Terv <bisilou@gmail.com>
2024-01-28 23:31:54 +0100

    corrected contrastive_loss_norm

--- loss.py
+++ loss.py
@@ -9,9 +9,9 @@ def contrastive_loss(v1: torch.Tensor, v2: torch.Tensor) -> torch.Tensor:
 
 def contrastive_loss_norm(v1: torch.Tensor, v2: torch.Tensor) -> torch.Tensor:
     CE = torch.nn.CrossEntropyLoss()
-    cosine_similarity_matrix=torch.nn.functional.cosine_similarity(v1,v2,dim=1)
-    labels = torch.arange(cosine_similarity_matrix.shape[0], device=v1.device)
-    return CE(cosine_similarity_matrix, labels) + CE(cosine_similarity_matrix.T, labels)
+    logits = torch.nn.functional.cosine_similarity(v1.unsqueeze(1), v2.unsqueeze(0), dim=2)
+    labels = torch.arange(logits.shape[0], device=v1.device)
+    return CE(logits, labels) + CE(torch.transpose(logits,0,1), labels)
 
 def tempered_contrastive_loss(v1: torch.Tensor, v2: torch.Tensor, temperature: torch.nn.Parameter) -> torch.Tensor:
     CE = torch.nn.CrossEntropyLoss()
