1e97ff21d73001d69ebafa65fc9b9b4267b93976
lea-khalil <leakhalil@yahoo.fr>
2024-01-29 16:08:52 +0100

    loss negative_sampling

--- loss.py
+++ loss.py
@@ -6,3 +6,12 @@ def contrastive_loss(v1: torch.Tensor, v2: torch.Tensor) -> torch.Tensor:
     logits = torch.matmul(v1, torch.transpose(v2, 0, 1))
     labels = torch.arange(logits.shape[0], device=v1.device)
     return CE(logits, labels) + CE(torch.transpose(logits, 0, 1), labels)
+
+BCEL = torch.nn.BCEWithLogitsLoss()
+
+def negative_sampling_contrastive_loss(v1, v2):
+    ##tentative à partir de la loss du papier, voir si ça marche mieux
+  logits = torch.matmul(v1,torch.transpose(v2, 0, 1))
+  labels = torch.arange(logits.shape[0], device=v1.device)
+  eye = torch.diag_embed(labels).to(v1.device)
+  return BCEL(logits, eye) + BCEL(torch.transpose(logits, 0, 1), eye), logits.diag() > 0
