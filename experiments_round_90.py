4744f0e61ecfab76ca4eeb6930f3f439699bc03b
lea-khalil <lea.khalil@yahoo.fr>
2024-01-29 15:03:06 +0100

    9008 more epochs Baz

--- experiments_round_90.py
+++ experiments_round_90.py
@@ -83,7 +83,7 @@ def get_round_90_experience(exp: int, configuration: dict, root_dir: Path = None
         model, configuration = generic_experiment(
             configuration,
             llm=DISTILBERT, graph=BIG_GCN,
-            n=200,
+            n=300,
             b=192, lr=3e-4, wd=1e-1,
             scheduler=PLATEAU, scheduler_configuration=dict(patience=10, factor=0.8),
             lora=False, quantization=None
