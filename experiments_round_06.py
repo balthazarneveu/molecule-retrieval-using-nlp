2e8526581641f2e8c5fbcc350cda99bcbc3118fa
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-31 14:25:58 +0100

    #18 exp 610 - retrain fat GCN from scratch

--- experiments_round_06.py
+++ experiments_round_06.py
@@ -6,6 +6,7 @@ from properties import (
 from pathlib import Path
 from properties import OUT_DIR
 from experiments_generic import generic_experiment, custom_lr
+from graph_model import FatGraphEncoder
 from functools import partial
 import torch
 from huggingface_hub import hf_hub_download
@@ -72,6 +73,30 @@ def get_round_6_experience(exp: int, configuration: dict, root_dir: Path = None,
             param.requires_grad = False
         configuration[SCHEDULER] = "ReduceLROnPlateau"
         configuration[SCHEDULER_CONFIGURATION] = dict(patience=8, factor=0.8)
+        configuration[NAME] += " Pretrained on 573"
+    if exp == 610:
+        # 573 LLM
+        lr = 1e-3
+        batch_size = 128
+        n = 200
+        model, configuration = generic_experiment(
+            configuration,
+            llm=SCIBERT, graph=FAT_GCN,
+            n=n,
+            b=batch_size, lr=lr, wd=1e-1,
+            lora=True, quantization=None,
+            temperature=False,
+        )
+        REPO_ID = "balthou/molnlp_0573_LoraSciBERT_FatGCN"
+        FILENAME = "model_0145.pt"
+        reload_model = hf_hub_download(repo_id=REPO_ID, filename=FILENAME, cache_dir=OUT_DIR)
+        model.load_state_dict(torch.load(reload_model)["model_state_dict"])
+        for param in model.text_encoder.bert.parameters():
+            param.requires_grad = False
+        graph_encoder = FatGraphEncoder(num_node_features=300, nout=768, nhid=512, graph_hidden_channels=512)
+        model.graph_encoder = graph_encoder
+        configuration[SCHEDULER] = "ReduceLROnPlateau"
+        configuration[SCHEDULER_CONFIGURATION] = dict(patience=5, factor=0.5)
         configuration[NAME] += " Pretrained on 573"
     print(configuration)
     return model, configuration
